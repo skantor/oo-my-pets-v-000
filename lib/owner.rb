274de6313718c6e1f553bb933431c8e01e9162cf
Sara Kantor <sara.kantor.nv@gmail.com>
2018-12-05 20:17:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
   # code goes here
+  
+  attr_accessor: @pets
+  
+  def initialize
+    @pets = {fishes:[],cats:[],dogs[]}
+  end 
+  
+  def buy_cat  
+  end 
+  
+  def buy_dog
+  end 
+
+  def walk_dogs(dog)
+  end 
+  
+  def play_with_cats(cat)
+  end
+  
+  def feed_fish(fish)
+  end 
+  
+  def sell_pets(pets)
+  end 
+  
+  def list_pets(pets)
+    @@pets 
+  end 
+
 end
